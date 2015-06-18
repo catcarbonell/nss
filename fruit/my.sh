4bfd9c6ec976f1e8f73e889c6a3e810c5d31aaee
Cathleen Carbonell <catcarbonell@gmail.com>
2015-06-18 16:59:17 -0700

    assignment has been saved under /fruit/my.sh
    will move

---
+++ fruit/my.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Bash project
+highlight="\033[34;43m"
+bold="\033[1m"
+normal="\033[0m]"
+freespace=$(df -h / grep - E "\/$" | awk '{print $4}')
+logdate=$(date +"%Y%m%d")
+logfile="$logdate"_report.log
+
+
+echo -e $bold"System Report:"$HOSTNAME"$normal
+printf "\tSystem type: \t%s\n" $MACHTYPE
+printf "\tBash Version:\t%s\n" $BASH_VERSION
+printf "\tFree Space:\T%s\n" $freespace
+echo -e $highlight"This system info summary has been saved to $logfile"$normal
+cat <<- EOF > $logfile
+
+printf "SYS:\t%s\n" $MACHTYPE >> $logfile
+printf "BASH:\t%s\n" $BASH_VERSION >> $logfile
+
+
